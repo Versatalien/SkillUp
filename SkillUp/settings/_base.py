5019ce4839d1f1b690e156ceb98e770ee6784a51
Macanthony Emmanuel <macanthonyemmanuel12@gmail.com>
2021-10-07 23:25:04 -0700

    Initial Commit*

--- SkillUp/settings/_base.py
+++ SkillUp/settings/_base.py
@@ -92,6 +92,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': get_secret("DATABASE_NAME"),
+        'USER':get_secret("DATABASE_USER"),
         'PASSWORD': get_secret("DATABASE_PASSWORD"),
         'HOST':'',
         'PORT':'',
--- SkillUp/settings/dev.py
+++ SkillUp/settings/dev.py
@@ -4,6 +4,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': get_secret("DATABASE_NAME"),
+        'USER':get_secret("DATABASE_USER"),
         'PASSWORD': get_secret("DATABASE_PASSWORD"),
         'HOST':'127.0.0.1',
         'PORT':'5432',
